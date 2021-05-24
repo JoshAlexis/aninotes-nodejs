2a65be005a818e59683930b612d550fb192f88c1
unknown <joshyelc@gmail.com>
2021-05-23 20:56:57 -0600

    Added JSDoc to functions

--- src/utils/paginated.js
+++ src/utils/paginated.js
@@ -1,4 +1,19 @@
-const paginated = ({
+/* eslint-disable no-unused-vars */
+const { Document } = require('mongoose');
+
+/**
+ * Creates an object with the data for pagination.
+ * @param {number} page - The number of the page.
+ * @param {number} limit - The amount of documents for the page.
+ * @param {number} startIndex - The index of the first element of the page.
+ * Also represents the amount to skipped elements.
+ * @param {number} endIndex - The index of the last element of the page.
+ * @param {Document[]} items - An array of Mongo documents
+ * @param {number} total - The amount of documents in the collection.
+ * @param {string} url - The base URL to create the URL's for the previous and next page.
+ * @returns An object with the structure of a paginated response.
+ */
+function paginated(
   page,
   limit,
   startIndex,
@@ -6,7 +21,7 @@ const paginated = ({
   items,
   total,
   url,
-}) => {
+) {
   const results = {};
 
   results.total = total;
@@ -37,6 +52,6 @@ const paginated = ({
   results.data = items;
 
   return results;
-};
+}
 
 module.exports = paginated;
